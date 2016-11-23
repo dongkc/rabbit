cab338a8e92c2dadc0fab50b02d988e10735a293
dongkc <dongkechang@gmail.com>
2016-11-23 13:13:18 +0800

    add baidu tts demo

---
+++ test/voice.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#curl -i 'https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=9cRIIIm9IeuQ9zMnS3p5guU8&client_secret=24fcc3fca9d60a600f94c889f762903e&'
+curl -i -v --data 'tex=前段时间，有家馄饨铺和一家葱油饼店影响很大。”11月21日在上海召开的深化“放管服”改革座谈会上，李克强说，“这两家小食店可能确实存在证照等问题，但我们基层政府部门也应更多百姓角度考虑一下，尽量寻求更多人的‘共赢’。&cuid=2e8a03f1fea94e6883c804a010c3f315&ctp=1&tok=24.ac7bf10adf6c0a32b51762c96d66d705.2592000.1482462975.282335-8649205&lan=zh' http://tsn.baidu.com/text2audio
